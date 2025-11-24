f4b98d00334f2cbc8979e6d42d7076ccbfadc2ca
Nakscott <gninphanna@gmail.com>
2025-11-24 14:38:03 +0700

    transfer code from command branch to bot branch

---
+++ src/t-bot/responses.py
@@ -0,0 +1,8 @@
+SUCCESS = "Expense added successfully!"
+MISSING_ARGUMENTS = "Missing arguments! Format: /add <category> <name> <price>"
+INVALID_PRICE = "Price must be a number!"
+EMPTY_LIST = "No expenses found."
+CHANGED = "Item updated!"
+REMOVED = "Item removed."
+ENDED = "Session ended. Goodbye!"
+
