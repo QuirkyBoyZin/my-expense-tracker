eee6ea4b063d84ef8bf6eb6320983768aeeb572f
Superkai017 <sashito015@gmail.com>
2025-11-26 12:07:14 +0700

    added script connected python to google sheet

--- src/g-sheets/sheets.py
+++ src/g-sheets/sheets.py
@@ -1,1 +1,21 @@
 from models.expense import Expense
+import os
+import gspread
+from google.oauth2.service_account import Credentials
+from googleapiclient.discovery import build
+from datetime import datetime
+
+import modify
+import retrieve
+
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+service_account_file = "Credential.json"
+creds = Credentials.from_service_account_file(service_account_file, scopes=SCOPES)
+sheet_id = "1VhkGo_GYGfHyWWM0Mt6Yr_UsJ-tE0wkTllxwFvhFL7M"
+client = gspread.authorize(creds)
+SHEET_GID = 0  # change if your tab has a different gid
+DATA_RANGE = "A2:F"
+service = build('sheets', 'v4', credentials=creds)
+sheet = service.spreadsheets()
+workbook = client.open_by_key(sheet_id)
+sheet_gspread = workbook.worksheet("expense_tracker")
