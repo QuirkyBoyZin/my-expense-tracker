e2aada1216d82e0749a03e672614c893d19861f5
Superkai017 <sashito015@gmail.com>
2025-11-26 13:25:37 +0700

    Modifed get_data_from function in retrieve.py

--- src/g-sheets/retrieve.py
+++ src/g-sheets/retrieve.py
@@ -1,4 +1,5 @@
 import sheets
+from datetime import datetime
 
 def retrieve_items() -> list:
     result = sheets.sheet.values().get(spreadsheetId=sheets.sheet_id,range = sheets.DATA_RANGE).execute()
@@ -6,9 +7,9 @@ def retrieve_items() -> list:
 def get_data(date: str):
     rows = retrieve_items()
     return [r for r in rows if len(r) > 1 and r[1] == date]
-def data_range(start_row: int, end_row: int):
-    s = sheets.datetime(start_row, "%Y-%m-%d" )
-    e = sheets.datetime(end_row, "%Y-%m-%d" )
+def data_range(start_date: str, end_date: str):
+    s = datetime(start_date, "%Y-%m-%d" )
+    e = datetime(end_date, "%Y-%m-%d" )
     while s <= e:
         yield s.strftime("%Y-%m-%d")
         s += sheets.timedelta(days=1)
@@ -21,22 +22,28 @@ def get_data_from(date1,date2):
            result[d] = filtered 
     return result
 def get_category(category):
-    row = retrieve_items()
+    rows = retrieve_items()
     price = []
-    for r in row:
+    for r in rows:
         if len(r) >= 6:
-            if row[3].lower() == category.lower():
+            if r[3].lower() == category.lower():
                 try:
                     price.append(float(r[5]))
                 except ValueError:
                     pass
     return price
-def get_item(date_str, id):
-    row = retrieve_items(date_str)
-    for r in row:
-        if row[0] == id:
-            return [ r[3] , r[4], r[5]]
+def get_item(date_str, item_id):
+    rows = get_data(date_str)   
+    for r in rows:
+        if r[0] == item_id:
+            return [r[3], r[4], r[5]]
     return None
+
 if __name__ == "__main__":
-    date = input("Enter date (YYYY-MM-DD): ")   
-    print(get_data(date))
+    #   date = input("Enter date (YYYY-MM-DD): ")
+    #   print(get_data(date))
+    #   item_id = input("Enter ID: ")
+    #   print(get_item(date, item_data
+    print(get_data_from("2025-11-26", "2025-11-27"))
+    
+
