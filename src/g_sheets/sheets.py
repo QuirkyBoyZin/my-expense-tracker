ea8b68f696635ce8296900d49ab07f4bea1c1256
Hour <hoursebastian@gmail.com>
2025-11-29 11:01:30 +0700

    Secured sheet's ID

--- src/g_sheets/sheets.py
+++ src/g_sheets/sheets.py
@@ -1,5 +1,6 @@
 from models.expense import Expense
 import os
+from dotenv import load_dotenv
 import gspread
 from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
@@ -7,11 +8,12 @@ from datetime import datetime
 
 import modify
 import retrieve
+load_dotenv()
 
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-service_account_file = "Credential.json"
+service_account_file = "credential.json"
 creds = Credentials.from_service_account_file(service_account_file, scopes=SCOPES)
-sheet_id = "1VhkGo_GYGfHyWWM0Mt6Yr_UsJ-tE0wkTllxwFvhFL7M"
+sheet_id = os.getenv("SHEET_ID")
 client = gspread.authorize(creds)
 SHEET_GID = 0  # change if your tab has a different gid
 DATA_RANGE = "A2:F"
